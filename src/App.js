0972bd520ec729f27296aa28b905e096020e7028
ErikUTA <erik4tohj@gmail.com>
2021-05-12 12:13:11 -0500

    Reto 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import Landing from './components/about/about';
+import { viewsEnum } from './components/constantes/constantes';
 import './App.css';
+import Excercises from './components/Excercises/Excercises';
 
 function App() {
+  const [view, setView] = useState(viewsEnum.LANDING);
+
+  const handleNavigation = (newView) => {
+    setView(newView);
+  }
+
+  let content = null;
+
+  switch (view) {
+    case viewsEnum.EXCERCISES:
+      content = <Excercises />;
+      break;
+    case viewsEnum.LANDING:
+    default:
+      content = <Landing />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header view={view} onNav={handleNavigation} />
+
+      {content}
     </div>
   );
 }
